589fe96d9a92886f13063782c0eaf3fb8669108e
unknowntpo <e850506@gmail.com>
2024-03-21 08:26:25 +0800

    revert(tmux): disable nvim-tmux-navigation

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,4 +1,5 @@
 -- copied from https://github.com/alexghergh/nvim-tmux-navigation
+--[[
 return {
   "alexghergh/nvim-tmux-navigation",
   config = function()
@@ -16,3 +17,24 @@ return {
     vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
   end,
 }
+--]]
+
+--[[
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
+--]]
